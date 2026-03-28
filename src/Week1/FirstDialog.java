f3ea684928fedf482646724b40ae1cd949300c2a
lqnguyen <lqnguyen190706@gmail.com>
2026-03-28 20:15:50 +0700

    update

---
+++ src/Week1/FirstDialog.java
@@ -0,0 +1,12 @@
+package Week1;
+
+public class FirstDialog {
+    import javax.swing.JOptionPane;
+
+    public class FirstDialog {
+        public static void main(String[] args){
+            JOptionPane.showMessageDialog(null,"Hello world! How are you?");
+            System.exit(0);
+        }
+    }
+}
