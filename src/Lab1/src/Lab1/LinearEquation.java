3410440ca36e739fd1e3e15cc9ce378394618df7
Lê Quang Nguyên <lqnguyen190706@gmail.com>
2026-03-28 21:02:17 +0700

    update

---
+++ src/Lab1/src/Lab1/LinearEquation.java
@@ -0,0 +1,32 @@
+import javax.swing.JOptionPane;
+
+public class LinearEquation {
+    public static void main(String[] args) {
+
+        String strA, strB;
+        double a, b;
+
+        strA = JOptionPane.showInputDialog("Enter a:");
+        strB = JOptionPane.showInputDialog("Enter b:");
+
+        a = Double.parseDouble(strA);
+        b = Double.parseDouble(strB);
+
+        String result;
+
+        if (a == 0) {
+            if (b == 0) {
+                result = "Infinite solutions";
+            } else {
+                result = "No solution";
+            }
+        } else {
+            double x = -b / a;
+            result = "Solution x = " + x;
+        }
+
+        JOptionPane.showMessageDialog(null, result);
+
+        System.exit(0);
+    }
+}
