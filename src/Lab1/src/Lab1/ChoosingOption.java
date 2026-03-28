58580c215e0d5d799a9a9e4073607a3a9f5434d5
Lê Quang Nguyên <lqnguyen190706@gmail.com>
2026-03-28 21:06:24 +0700

    Add ChoosingOption class with confirmation dialog

---
+++ src/Lab1/src/Lab1/ChoosingOption.java
@@ -0,0 +1,16 @@
+import javax.swing.JOptionPane;
+
+public class ChoosingOption {
+    public static void main(String[] args) {
+
+        int option = JOptionPane.showConfirmDialog(
+                null,
+                "Do you want to change to the first class ticket?"
+        );
+
+        JOptionPane.showMessageDialog(null,
+                "You've chosen: " + option);
+
+        System.exit(0);
+    }
+}
