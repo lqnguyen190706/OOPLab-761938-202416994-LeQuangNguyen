15f7b620c0f98c6cca2776564630e92b1cc47c41
Lê Quang Nguyên <lqnguyen190706@gmail.com>
2026-03-28 21:11:18 +0700

    Add TriangleStars class to print star triangles

---
+++ src/Lab1/src/Lab1/TriangleStars.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class TriangleStars {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.print("Enter the height (n): ");
+        int n = keyboard.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            int stars = 2 * i - 1;
+
+            for (int j = 1; j <= stars; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println(); // move to next line
+        }
+
+        keyboard.close();
+    }
+}
