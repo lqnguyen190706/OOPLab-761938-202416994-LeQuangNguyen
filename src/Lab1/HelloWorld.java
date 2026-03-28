6762f2a7947c453ff904744e2b3b0261512e203c
lqnguyen <lqnguyen190706@gmail.com>
2026-03-28 20:33:02 +0700

    first commit

--- src/Lab1/HelloWorld.java
+++ src/Lab1/HelloWorld.java
@@ -1,7 +1,7 @@
 package Lab1;
 
-public class HelloWorld{
-    public static void main(String args[]){
+public class HelloWorld {
+    public static void main(String args[]) {
         System.out.println("Xin chao \n cac ban");
         System.out.println("Hello \t world!");
     }
