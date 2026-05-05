b1578198f256b08ffa3e3f8aceabd569614a0634
Lê Quang Nguyên <lqnguyen190706@gmail.com>
2026-05-05 16:15:02 +0700

    update

--- src/Lab02/DigitalVideoDisc.java
+++ src/Lab02/DigitalVideoDisc.java
@@ -2,6 +2,11 @@ package Lab02;
 
 public class DigitalVideoDisc {
 
+    // Class member (dùng chung)
+    private static int nbDigitalVideoDiscs = 0;
+
+    // Instance member
+    private int id;
 
     private String title;
     private String category;
@@ -9,32 +14,49 @@ public class DigitalVideoDisc {
     private int length;
     private float cost;
 
-
+    // Constructor 1
     public DigitalVideoDisc(String title) {
         this.title = title;
+        updateId();
     }
 
+    // Constructor 2
     public DigitalVideoDisc(String category, String title, float cost) {
         this.category = category;
         this.title = title;
         this.cost = cost;
+        updateId();
     }
 
+    // Constructor 3
     public DigitalVideoDisc(String director, String category, String title, float cost) {
         this.director = director;
         this.category = category;
         this.title = title;
         this.cost = cost;
+        updateId();
     }
 
+    // Constructor 4
     public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
         this.title = title;
         this.category = category;
         this.director = director;
         this.length = length;
         this.cost = cost;
+        updateId();
     }
 
+    // Hàm cập nhật id
+    private void updateId() {
+        nbDigitalVideoDiscs++;
+        this.id = nbDigitalVideoDiscs;
+    }
+
+    // Getter
+    public int getId() {
+        return id;
+    }
 
     public String getTitle() {
         return title;
@@ -55,6 +77,7 @@ public class DigitalVideoDisc {
     public float getCost() {
         return cost;
     }
+
     public void setTitle(String title) {
         this.title = title;
     }
